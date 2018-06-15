b933152dee0f206cd9fdaf7e501b66d2ab4a1832
Your Name <you@example.com>
2018-06-16 01:48:57 +0700

    update

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/webhook', (req, res) => {
         var sql = "SELECT * FROM user WHERE Rendom = '" + msg + "' ";
         con.query(sql, function (err, result, fields) {
             if (err) throw err;
-            var sqli = "UPDATE user SET Key = '" + user_id + "' WHERE Rendom = '" + msg + "' ";
+            var sqli = "UPDATE user SET Key = '" + user_id + "' WHERE ID = '23' ";
             con.query(sqli, function (err, result, fields) {
 
 
